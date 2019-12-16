f3c7f741cfd6b6dddf7583ea20d02a4313eb17ba
Leopold Talirz <leopold.talirz@gmail.com>
2019-12-16 17:09:45 +0100

    try fixing travis build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,9 @@ from aiida.manage.manager import get_manager
 # Enable rtd mode via `export READTHEDOCS=True`
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
+if on_rtd:  # let RTD set the theme
+    pass
+else:
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
